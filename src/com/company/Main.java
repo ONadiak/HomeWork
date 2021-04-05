c8e950a066b6a6efaa058e3caf155dca27e0f508
ONadiak <nadiak200202@gmail.com>
2021-04-05 10:56:36 +0300

    Task 2 Done

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+
+
+    public static void main(String[] args)
+    	{
+
+
+    int n = functions.Input(); // функция ввода числа
+	functions.Check_3(n); // провірка на наявність " 3 " в квадраті заданого числа
+	functions.Inverting(n); // Перестановка заданого числа
+	functions.Reshuffle(n); // Перестановка першої і останньої цифри заданого числа
+	functions.Adding_1(n); // добавлення одиничок на початок і кінець
+    	}
+
+
+}
